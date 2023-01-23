43903d2960e7459734b2dadc58c967fa2779c58a
jmore15 <jonmoreira15@outlook.com>
2023-01-22 21:56:07 -0600

    Creación del proyecto inicial

---
+++ src/main/java/com/TiendaJ_2023/TiendaJ2023Application.java
@@ -0,0 +1,13 @@
+package com.TiendaJ_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaJ2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaJ2023Application.class, args);
+	}
+
+}
